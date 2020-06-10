14af287d316aafbaca3220b120ddf2563f372330
Ian Gregg <iangregg188@gmail.com>
2020-06-10 14:14:24 -0700

    updated the submit function to show the output text translation.
    Co-authored by Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,30 @@
 //Business logic
 
-
-
-//UI logic
-$(document).ready(function(){
-  $("form#sentence").submit(function(event) {
-    event.preventDefault ();
-    const inputSentence = $("input#sentence1").val();
-
-    function transpigLatin (inputSentence) {
-      let vowels = ['a','e','i','o','u']
-      let newStr = ("")
-      if (vowels.indexOf(inputSentence[0]) > -1) {
+function transpigLatin (inputSentence) {
+  let vowels = ['a','e','i','o','u']
+  let newStr = ("")
+  if (vowels.indexOf(inputSentence) > -1) {
         newStr =  inputSentence + "way";
         return newStr;
-      } 
-      else {
-        let firstMatch = inputSentence.match([a,e,i,o,u]) || 0;
+  }
+  else {
+        let firstMatch = inputSentence.match(/[aeiou]/g) || 0;
         let vowel = inputSentence.indexOf(firstMatch [0]);
         newStr = inputSentence.substring(vowel) + inputSentence.substring(0, vowel) + "ay";
         return newStr;
       }
-    };
+  };
+//UI logic
+$(document).ready(function(){
+  $("form#sentence").submit(function(event) {
+    event.preventDefault ();
+    let inputSentence = $("input#sentence1").val();
+    let result = transpigLatin(inputSentence);
+
+    $("p#output").text(result);
+        //return newStr;
     //if (inputSentence[i] = a; inputSentence[i = e; )
         //.push("way")
     //return 
-    $("p#output").print(newStr);
   });
 });
