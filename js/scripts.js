62950320895ca94a1ede2d9a9124830c1133e81f
Ian Gregg <iangregg188@gmail.com>
2020-06-10 13:34:25 -0700

    Worked on applying the DRY method to the codeto simplify it logic.
    Co-authored by Emma Hellman <ehh801@icloud.com>

--- js/scripts.js
+++ js/scripts.js
@@ -7,21 +7,24 @@ $(document).ready(function(){
   $("form#sentence").submit(function(event) {
     event.preventDefault ();
     const inputSentence = $("input#sentence1").val();
-    let vowels = ['a','e','i','o','u']
-    let newStr = (""),
 
-    if (vowels.indexOf(inputSentence[0])>0){
-      newStr = str + "way"
-      return newStr
-    }
-    else {
-
-    }
+    function transpigLatin (inputSentence) {
+      let vowels = ['a','e','i','o','u']
+      let newStr = ("")
+      if (vowels.indexOf(inputSentence[0]) > -1) {
+        newStr =  inputSentence + "way";
+        return newStr;
+      } 
+      else {
+        let firstMatch = inputSentence.match([a,e,i,o,u]) || 0;
+        let vowel = inputSentence.indexOf(firstMatch [0]);
+        newStr = inputSentence.substring(vowel) + inputSentence.substring(0, vowel) + "ay";
+        return newStr;
+      }
+    };
     //if (inputSentence[i] = a; inputSentence[i = e; )
         //.push("way")
     //return 
-    //}
-
-
+    $("p#output").print(newStr);
   });
 });
